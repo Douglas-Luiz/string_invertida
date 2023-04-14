0cb64c3578fcab15005347653350e4941665db31
Douglas Luiz <105089820+Douglas-Luiz@users.noreply.github.com>
2023-04-14 19:19:18 -0300

    atualização

--- string.js
+++ string.js
@@ -4,7 +4,7 @@ let palavra = prompt("Insira uma palavra !")
 let arr = []
 for (let i = palavra.length - 1; i >= 0; i--) {
 
-    var rs = palavra[i];
+    let rs = palavra[i];
     arr.push(rs)
  
 
